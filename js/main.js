438d34ad7df122eb302800526374d1e9325039c6
Gabriel dos Santos Oliveira <gabrielsantosoliveira951@gmail.com>
2022-08-31 11:18:59 -0400

    Feito uma pwa que calcula o imc

---
+++ js/main.js
@@ -0,0 +1,28 @@
+window.onload = () => {
+  "use strict";
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.register("./sw.js");
+  }
+};
+function calculatorImc() {
+  const peso = document.getElementById("peso").value;
+  const altura = document.getElementById("altura").value;
+  const result = Math.round(peso / (altura * altura));
+  let categoria;
+  if (result < 18.5) {
+    categoria = "Abaixo do peso normal"
+  } else if (result > 18.5 && result < 24.9) {
+    categoria = "Peso normal"
+  } else if (result > 25 && result < 29.9) {
+    categoria = "Excesso de peso"
+  } else if (result > 30 && result < 34.9) {
+    categoria = "Obesidade classe 1"
+
+  } else if (result > 35 && result < 39.9) {
+    categoria = "Obesidade classe 2"
+  } else {
+    categoria = "Obesidade classe 3"
+  }
+  document.getElementById('result').innerHTML = "<div>Sua categoria é: " + categoria + '</div>O seu peso é ' + result;
+
+}
